dae64fde55c0aad8e6af01c846db6598375a318d
erich oliphant <erich.oliphant@gmail.com>
2016-02-10 00:18:51 -0500

    fixed issue where CS.update fails if countries.yml doesn't already exist

--- lib/city-state.rb
+++ lib/city-state.rb
@@ -37,7 +37,7 @@ module CS
       self.install(state_fn.split(".").last.upcase.to_sym) # reinstall country
     end
     @countries, @states, @cities = [{}, {}, {}] # invalidades cache
-    File.delete COUNTRIES_FN # force countries.yml to be generated at next call of CS.countries
+    File.delete COUNTRIES_FN if File.exist? COUNTRIES_FN # force countries.yml to be generated at next call of CS.countries
     true
   end
 
